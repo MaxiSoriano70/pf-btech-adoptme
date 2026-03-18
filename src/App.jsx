6cdf78bebef5920c5fb00af46b2c7467516ae316
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2026-03-18 07:58:23 -0300

    Se agrego lo de usuario anonimo al app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import './App.css'
-import RoutesViews from './routes/RoutesViews'
+import './App.css';
+import RoutesViews from './routes/RoutesViews';
+import { useEffect, useState } from "react";
+import { signInAnonymously, onAuthStateChanged } from "firebase/auth";
+import { auth } from './firebase/config';
+import LoadingPage from './components/LoadingPage';
 
 function App() {
 
-  return (
-    <RoutesViews/>
-  )
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    signInAnonymously(auth)
+      .catch((error) => {
+        console.error("Error auth:", error);
+      });
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("Usuario listo:", user.uid);
+        setLoading(false);
+      }
+    });
+
+    return () => unsubscribe();
+
+  }, []);
+
+  if (loading) return <LoadingPage/>;
+
+  return <RoutesViews />;
 }
 
-export default App
+export default App;
